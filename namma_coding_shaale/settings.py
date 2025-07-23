68703d851adcae803c6e887b0472e0246fece3cc
Ravichandran T S <76648986+ravichandran202@users.noreply.github.com>
2025-07-23 14:37:11 +0530

    Update settings.py

--- namma_coding_shaale/settings.py
+++ namma_coding_shaale/settings.py
@@ -161,7 +161,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://namma-coding-shaale.onrender.com"
+    "https://namma-coding-shaale.onrender.com",
+    "https://nammacodingshaale.up.railway.app
 ]
 
 # Allow cookies (if needed)
