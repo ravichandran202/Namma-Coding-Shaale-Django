594ca5b5184ed226c260823f345e1fbfd502fd02
Ravichandran T S <76648986+ravichandran202@users.noreply.github.com>
2025-10-02 01:15:27 +0530

    fix settings.py

--- namma_coding_shaale/settings.py
+++ namma_coding_shaale/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "namma_coding_shaale_app",
+    "django.contrib.humanize",
     'rest_framework',
     'corsheaders',
     'login_history'
